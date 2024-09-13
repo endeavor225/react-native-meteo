40682a5fbfa07f94cd1a8c76e3643abff7985d14
Abraham KONE <kone.abraham@jool-int.com>
2024-09-13 15:24:59 +0000

    Navigation : Mise en place

--- App.js
+++ App.js
@@ -4,24 +4,44 @@ import { ImageBackground } from "react-native";
 import backgroundImg from "./assets/background.png";
 import AlataRegular from "./assets/fonts/Alata-Regular.ttf";
 import { useFonts } from "expo-font";
-
 import { s } from "./App.style";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Forecast } from "./pages/Forecast/Forecast";
+const Stack = createNativeStackNavigator();
+
+const navTheme = {
+  colors: {
+    background: "transparent",
+  },
+};
 export default function App() {
   const [isFontLoaded] = useFonts({
     "Alata-Regular": AlataRegular,
   });
+  
 
   return (
-    <ImageBackground
+    <NavigationContainer theme={navTheme}>
+      <ImageBackground
       source={backgroundImg}
       style={s.img_background}
       imageStyle={s.img}
     >
       <SafeAreaProvider>
         <SafeAreaView style={s.container}>
-          {isFontLoaded ? <Home /> : null}
+          {isFontLoaded ? 
+            <Stack.Navigator 
+              initialRouteName="Forecast" 
+              screenOptions={{ headerShown: false }}
+            >
+              <Stack.Screen name="Home" component={Home} />
+              <Stack.Screen name="Forecast" component={Forecast} />
+            </Stack.Navigator> : null}
         </SafeAreaView>
       </SafeAreaProvider>
     </ImageBackground>
+    </NavigationContainer>
+    
   );
 }
---
+++ pages/Forecast/Forecast.jsx
@@ -0,0 +1,10 @@
+import { Txt } from "../../components/Txt/Txt.jsx";
+import { s } from "./Forecast.style.js";
+
+export function Forecast({}) {
+  return (
+    <>
+      <Txt>Forecast</Txt>
+    </>
+  );
+}
