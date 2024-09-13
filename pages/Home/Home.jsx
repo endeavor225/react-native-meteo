95be3366038f34ab5364b6ef81786ad13ff2402b
Abraham KONE <kone.abraham@jool-int.com>
2024-09-13 10:49:55 +0000

    Accéder aux coordonnées

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,14 +1,36 @@
 import { Text, View } from "react-native";
 import { s } from "./Home.style";
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from "expo-location";
+import { useEffect, useState } from "react";
+
 
 export function Home({}) {
-  return (
-    <>
-      <View style={s.meteo_basic}>
-        <Text style={{ fontSize: 60, color: "white" }}>Hello</Text>
-      </View>
-      <View style={s.searchbar_container}></View>
-      <View style={s.meteo_advanced}></View>
-    </>
-  );
+    const [coords, setCoords] = useState()
+
+    useEffect(() => {
+        getUserCoords()
+    }, [])
+
+    async function getUserCoords() {
+        let { status } = await requestForegroundPermissionsAsync();
+        if (status === "granted") {
+          const location = await getCurrentPositionAsync();
+          setCoords({
+            lat: location.coords.latitude,
+            lng: location.coords.longitude,
+          });
+        } else {
+          setCoords({ lat: "48.85", lng: "2.35" });
+        }
+    }
+    
+    return (
+        <>
+        <View style={s.meteo_basic}>
+            <Text style={{ fontSize: 60, color: "white" }}>Hello</Text>
+        </View>
+        <View style={s.searchbar_container}></View>
+        <View style={s.meteo_advanced}></View>
+        </>
+    );
 }
