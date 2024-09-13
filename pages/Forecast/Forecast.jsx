1936e49e29257a012cdd293ffc3acddbd8244ce1
Abraham KONE <kone.abraham@jool-int.com>
2024-09-13 16:26:02 +0000

    Header et retour arrière

--- pages/Forecast/Forecast.jsx
+++ pages/Forecast/Forecast.jsx
@@ -1,13 +1,28 @@
 import { Container } from "../../components/Container/Container.jsx";
 import { Txt } from "../../components/Txt/Txt.jsx";
 import { s } from "./Forecast.style.js";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { TouchableOpacity, View } from "react-native";
+
 export function Forecast({}) {
   const { params } = useRoute();
-  console.log("*** params !", params);
-  return (
-    <Container>
-      <Txt>Forecast</Txt>
-    </Container>
+  const nav = useNavigation();
+
+  const backButton = (
+    <TouchableOpacity style={s.back_btn} onPress={() => nav.goBack()}>
+      <Txt>{"<"}</Txt>
+    </TouchableOpacity>
   );
+  const header = (
+    <>
+      <View style={s.header}>
+        {backButton}
+        <View style={s.header_texts}>
+          <Txt>{params.city}</Txt>
+          <Txt style={s.subtitle}>Prévision sur 7 jours</Txt>
+        </View>
+      </View>
+    </>
+  );
+  return <Container>{header}</Container>;
 }
