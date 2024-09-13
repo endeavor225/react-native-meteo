880034f57c90bb21c542d1372956b5ceb70893dd
Abraham KONE <kone.abraham@jool-int.com>
2024-09-13 16:34:38 +0000

    Ligne de prévision

--- pages/Forecast/Forecast.jsx
+++ pages/Forecast/Forecast.jsx
@@ -1,8 +1,9 @@
 import { Container } from "../../components/Container/Container.jsx";
 import { Txt } from "../../components/Txt/Txt.jsx";
 import { s } from "./Forecast.style.js";
-import { useNavigation, useRoute } from "@react-navigation/native";
+import { useRoute, useNavigation } from "@react-navigation/native";
 import { TouchableOpacity, View } from "react-native";
+import { ForecastListItem } from "../../components/ForecastListItem/ForecastListItem.jsx";
 
 export function Forecast({}) {
   const { params } = useRoute();
@@ -10,19 +11,66 @@ export function Forecast({}) {
 
   const backButton = (
     <TouchableOpacity style={s.back_btn} onPress={() => nav.goBack()}>
-      <Txt>{"<"}</Txt>
+      <Txt> {"<"} </Txt>
     </TouchableOpacity>
   );
   const header = (
-    <>
-      <View style={s.header}>
-        {backButton}
-        <View style={s.header_texts}>
-          <Txt>{params.city}</Txt>
-          <Txt style={s.subtitle}>Prévision sur 7 jours</Txt>
-        </View>
+    <View style={s.header}>
+      {backButton}
+      <View style={s.header_texts}>
+        <Txt>{params.city}</Txt>
+        <Txt style={s.subtitle}>Prévisions sur 7 jours</Txt>
       </View>
-    </>
+    </View>
   );
-  return <Container>{header}</Container>;
+
+  return (
+    <Container>
+      {header}
+      <View style={{ marginTop: 50 }}>
+        <ForecastListItem
+          image={require("../../assets/clouds.png")}
+          day="LUN"
+          date="03/11/2023"
+          temperature={14}
+        />
+        <ForecastListItem
+          image={require("../../assets/clouds.png")}
+          day="LUN"
+          date="03/11/2023"
+          temperature={14}
+        />
+        <ForecastListItem
+          image={require("../../assets/clouds.png")}
+          day="LUN"
+          date="03/11/2023"
+          temperature={14}
+        />
+        <ForecastListItem
+          image={require("../../assets/clouds.png")}
+          day="LUN"
+          date="03/11/2023"
+          temperature={14}
+        />
+        <ForecastListItem
+          image={require("../../assets/clouds.png")}
+          day="LUN"
+          date="03/11/2023"
+          temperature={14}
+        />
+        <ForecastListItem
+          image={require("../../assets/clouds.png")}
+          day="LUN"
+          date="03/11/2023"
+          temperature={14}
+        />
+        <ForecastListItem
+          image={require("../../assets/clouds.png")}
+          day="LUN"
+          date="03/11/2023"
+          temperature={14}
+        />
+      </View>
+    </Container>
+  );
 }
